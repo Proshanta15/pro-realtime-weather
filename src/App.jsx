02ba756fd7c937b9b26236bb7aad4e8a017c1ac8
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2025-09-17 18:25:18 +0600

    weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <h1>Weather app</h1>
-      </div>
+      <Header />
     </>
   );
 }
