a0c61d99ce949c4130c193c277c4e2a05867aee8
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2025-09-21 19:07:08 +0600

    pro-weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <section className="flex justify-center h-[90vh] items-center">
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 }
