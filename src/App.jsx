de0c26a4219e244b6b01f8a65800ffd647080402
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2025-09-28 16:29:44 +0600

    weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
+import { WeatherProvider } from "./provider";
 
 function App() {
   return (
     <>
-      <Header />
-      <main>
-        <section className="flex justify-center h-[90vh] items-center">
-          <WeatherBoard />
-        </section>
-      </main>
+      <WeatherProvider>
+        <Header />
+        <main>
+          <section className="flex justify-center h-[90vh] items-center">
+            <WeatherBoard />
+          </section>
+        </main>
+      </WeatherProvider>
     </>
   );
 }
