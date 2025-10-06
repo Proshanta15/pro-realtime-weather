25095f33dca5acdace9070ec47c0fac2272d186f
Proshanta Kumar Roy <Proshanta.ict@pakiza.com>
2025-10-06 16:31:25 +0600

    weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
-import { WeatherProvider } from "./provider";
+import { FavouriteProvider, WeatherProvider } from "./provider";
 
 function App() {
   return (
     <>
       <WeatherProvider>
-        <Header />
-        <main>
-          <section className="flex justify-center h-[90vh] items-center">
-            <WeatherBoard />
-          </section>
-        </main>
+        <FavouriteProvider>
+          <Header />
+          <main>
+            <section className="flex justify-center h-[90vh] items-center">
+              <WeatherBoard />
+            </section>
+          </main>
+        </FavouriteProvider>
       </WeatherProvider>
     </>
   );
--- src/components/weather/AddToFavourite.jsx
+++ src/components/weather/AddToFavourite.jsx
@@ -1,11 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import HeartRedIcon from "../../assets/heart-red.svg";
 import HeartIcon from "../../assets/heart.svg";
+import { FavouriteContext, WeatherContext } from "../../context";
 export default function AddToFavourite() {
+  const { addToFavourite, removeFromFavourites, favourites } =
+    useContext(FavouriteContext);
+
+  const { weatherData } = useContext(WeatherContext);
+
+  const { latitude, longitude, location } = weatherData;
+
+  const [isFavourite, toggleFavourite] = useState(false);
+
+  useEffect(() => {
+    const found = favourites.find((fav) => fav.location === location);
+    toggleFavourite(found);
+  }, []);
+
+  function handleFavourite() {
+    const found = favourites.find((fav) => fav.location === location);
+    if (!found) {
+      addToFavourite(latitude, longitude, location);
+    } else {
+      removeFromFavourites(location);
+    }
+    toggleFavourite(!isFavourite);
+  }
+
   return (
     <div className="md:col-span-2">
       <div className="flex items-center justify-end space-x-6">
-        <button className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D] cursor-pointer">
+        <button
+          className="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D] cursor-pointer"
+          onClick={handleFavourite}
+        >
           <span>Add to Favourite</span>
-          <img src={HeartIcon} alt="" />
+          <img src={isFavourite ? HeartRedIcon : HeartIcon} alt="Heart" />
         </button>
       </div>
     </div>
